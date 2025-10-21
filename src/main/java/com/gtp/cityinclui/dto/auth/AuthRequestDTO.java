ff9bebe79c6b97a7eb7f06b4cf50501ede0b23ec
Nemoske <luandevfreitas7@gmail.com>
2025-10-21 14:41:31 -0300

    feat: add DTOs for authentication request and response

---
+++ src/main/java/com/gtp/cityinclui/dto/auth/AuthRequestDTO.java
@@ -0,0 +1,54 @@
+package com.gtp.cityinclui.dto.auth;
+
+import java.util.Objects;
+
+public class AuthRequestDTO {
+
+    private String email;
+    private String senha;
+
+    public AuthRequestDTO() {
+    }
+
+    public AuthRequestDTO(String email, String senha) {
+        this.email = email;
+        this.senha = senha;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    @Override
+    public String toString() {
+        return "AuthRequestDTO{" +
+                "email='" + email + '\'' +
+                ", senha='" + senha + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AuthRequestDTO that = (AuthRequestDTO) o;
+        return Objects.equals(email, that.email) && Objects.equals(senha, that.senha);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(email, senha);
+    }
+}
---
+++ src/main/java/com/gtp/cityinclui/dto/auth/AuthResponseDTO.java
@@ -0,0 +1,43 @@
+package com.gtp.cityinclui.dto.auth;
+
+import java.util.Objects;
+
+public class AuthResponseDTO {
+
+    private String token;
+
+    public AuthResponseDTO() {
+    }
+
+    public AuthResponseDTO(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    @Override
+    public String toString() {
+        return "AuthResponseDTO{" +
+                "token='" + token + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AuthResponseDTO that = (AuthResponseDTO) o;
+        return Objects.equals(token, that.token);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(token);
+    }
+}
