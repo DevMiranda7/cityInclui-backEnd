bc1c29958c7fafbeb36c0b91d1dec9abef5dd804
Nemoske <luandevfreitas7@gmail.com>
2025-10-28 16:20:16 -0300

    refactor: remove method deleteAll

--- src/main/java/com/gtp/cityinclui/repository/PhotoRepository.java
+++ src/main/java/com/gtp/cityinclui/repository/PhotoRepository.java
@@ -3,11 +3,7 @@ package com.gtp.cityinclui.repository;
 import com.gtp.cityinclui.entity.PhotoRegister;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
-
 public interface PhotoRepository extends ReactiveCrudRepository<PhotoRegister, Long > {
 
     Flux<PhotoRegister> findByOwnerId(Long ownerId);
-
-    Mono<Void> deleteAllByOwnerId(Long ownerId);
 }
