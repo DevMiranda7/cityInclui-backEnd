071664724c4cfc7bfbb9d2e4409b50ef012c3c79
Emerson Leandro <emersonleandrosilva0@gmail.com>
2025-11-04 01:21:54 -0300

    Criação ResponseClientDTO

--- src/main/java/com/gtp/cityinclui/dto/client/ResponseClientDTO.java
+++ src/main/java/com/gtp/cityinclui/dto/client/ResponseClientDTO.java
@@ -1,5 +1,8 @@
 package com.gtp.cityinclui.dto.client;
 
 public record ResponseClientDTO (
-
+    Long id, // Ou String, dependendo do tipo do seu ID no banco
+    String nomeCompleto,
+    String telefone,
+    String email
 ){}
